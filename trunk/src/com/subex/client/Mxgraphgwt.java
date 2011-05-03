16b8c447598f8d211626320837d34237bc29b378
senguptasourish@gmail.com <senguptasourish@gmail.com@8d2d0ade-4398-c17c-fa17-588cd22d8ca3>
2011-05-03 19:17:41 +0000

    Added window support.

--- trunk/src/com/subex/client/Mxgraphgwt.java
+++ trunk/src/com/subex/client/Mxgraphgwt.java
@@ -8,19 +8,23 @@ import com.extjs.gxt.ui.client.event.Listener;
 import com.extjs.gxt.ui.client.event.WidgetListener;
 import com.extjs.gxt.ui.client.widget.Component;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
+import com.extjs.gxt.ui.client.widget.Document;
 import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.button.ToolButton;
+import com.extjs.gxt.ui.client.widget.form.ComboBox;
+import com.extjs.gxt.ui.client.widget.form.TextField;
 import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.subex.client.mxgraph.Canvas;
 import com.subex.client.mxgraph.CanvasConstants;
+import com.subex.client.mxgraph.MxWindow;
 import com.subex.client.mxgraph.Node;
 
 @SuppressWarnings("unchecked")
 public class Mxgraphgwt implements EntryPoint {
-	
+
 	Canvas canvas;
 
 	public void onModuleLoad() {
@@ -53,7 +57,7 @@ public class Mxgraphgwt implements EntryPoint {
 			}
 		});
 		toolBar.add(zoomIn);
-		
+
 		Button zoomOut = new Button("-");
 		zoomOut.addListener(Events.OnClick, new Listener<BaseEvent>() {
 			public void handleEvent(BaseEvent be) {
@@ -61,7 +65,7 @@ public class Mxgraphgwt implements EntryPoint {
 			}
 		});
 		toolBar.add(zoomOut);
-		
+
 		Button zoomReset = new Button("1:1");
 		zoomReset.addListener(Events.OnClick, new Listener<BaseEvent>() {
 			public void handleEvent(BaseEvent be) {
@@ -69,7 +73,7 @@ public class Mxgraphgwt implements EntryPoint {
 			}
 		});
 		toolBar.add(zoomReset);
-		
+
 		Button fit = new Button("Fit");
 		fit.addListener(Events.OnClick, new Listener<BaseEvent>() {
 			public void handleEvent(BaseEvent be) {
@@ -77,7 +81,7 @@ public class Mxgraphgwt implements EntryPoint {
 			}
 		});
 		toolBar.add(fit);
-		
+
 		Button show = new Button("show");
 		show.addListener(Events.OnClick, new Listener<BaseEvent>() {
 			public void handleEvent(BaseEvent be) {
@@ -85,7 +89,7 @@ public class Mxgraphgwt implements EntryPoint {
 			}
 		});
 		toolBar.add(show);
-		
+
 		Button print = new Button("print");
 		print.addListener(Events.OnClick, new Listener<BaseEvent>() {
 			public void handleEvent(BaseEvent be) {
@@ -94,7 +98,37 @@ public class Mxgraphgwt implements EntryPoint {
 		});
 		toolBar.add(print);
 
+		Button addBtn = new Button("Add");
+		addBtn.addListener(Events.OnClick, new Listener<BaseEvent>() {
+			public void handleEvent(BaseEvent be) {
+				showWindow();
+			}
+		});
+		toolBar.add(addBtn);
+
 		return toolBar;
+	}
+
+	private void showWindow() {
+		MxWindow window = new MxWindow();
+		window.setTitle("sourish");
+		window.setXPos(300);
+		window.setYPos(300);
+		window.setHeight(200);
+		window.setWidth(200);
+		window.setContentId("ssg");
+		
+		window.show();
+		
+		com.google.gwt.dom.client.Document.get().getElementById("ssg").appendChild(new ComboBox().getElement());
+	}
+
+	private ContentPanel getContent() {
+		ContentPanel panel = new ContentPanel();
+		panel.setId("panel");
+		panel.add(new TextField());
+		
+		return panel;
 	}
 
 	private void drawCanvas() {
