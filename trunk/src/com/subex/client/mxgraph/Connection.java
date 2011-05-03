f7914368b9b12389083c4d15a6394b988e385c9c
senguptasourish@gmail.com <senguptasourish@gmail.com@8d2d0ade-4398-c17c-fa17-588cd22d8ca3>
2011-05-03 14:23:18 +0000

    Initial check-in of basic lib

---
+++ trunk/src/com/subex/client/mxgraph/Connection.java
@@ -0,0 +1,16 @@
+package com.subex.client.mxgraph;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+public class Connection extends CanvasItem
+{
+	public Connection( Node source, Node target )
+	{
+		id = source.id + "_" + target.id;
+		setJsObject( createConnection( id, "", source.getJsObject(), target.getJsObject() ) );
+	}
+
+	private static native JavaScriptObject createConnection( String id, String label, JavaScriptObject source, JavaScriptObject target ) /*-{
+		return $wnd.createConnection( id, label, source, target );
+	}-*/;
+}
---
+++ trunk/src/com/subex/client/mxgraph/Node.java
@@ -0,0 +1,21 @@
+package com.subex.client.mxgraph;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+public class Node extends CanvasItem
+{
+	Node( String id, String label, int xPos, int yPos, int width, int height )
+	{
+		super.id = id;
+		setJsObject( createNode( id, label, xPos, yPos, width, height ) );
+	}
+
+	public void setStyle(String styleName, String styleValue)
+	{
+		Canvas.setStyle(getJsObject(), styleName, styleValue);
+	}
+	
+	private static native JavaScriptObject createNode( String id, String label, int xPos, int yPos, int width, int height ) /*-{
+		return $wnd.createNode( id, label, xPos, yPos, width, height );
+	}-*/;
+}
