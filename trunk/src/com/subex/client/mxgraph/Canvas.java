36a2c815ddcf10125391a0da442eca6400661b7e
senguptasourish@gmail.com <senguptasourish@gmail.com@8d2d0ade-4398-c17c-fa17-588cd22d8ca3>
2011-05-04 14:49:03 +0000

    Outline

--- trunk/src/com/subex/client/mxgraph/Canvas.java
+++ trunk/src/com/subex/client/mxgraph/Canvas.java
@@ -5,65 +5,66 @@ import com.extjs.gxt.ui.client.event.WidgetListener;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.google.gwt.core.client.JavaScriptObject;
 
-public class Canvas<T> extends ContentPanel
-{
+public class Canvas<T> extends ContentPanel {
 	private JavaScriptObject jsObject;
 	private boolean widgetAttached;
 
-	public Canvas( final String id )
-	{		
+	public Canvas(final String id, final CanvasIntializeListener listener) {
 		setId(id);
-		createCanvas( getId() );
-		addWidgetListener( new WidgetListener()
-		{
-			public void widgetAttached( ComponentEvent ce )
-			{
-				jsObject = createCanvas( getId() );
+		// createCanvas( getId() );
+		addWidgetListener(new WidgetListener() {
+			public void widgetAttached(ComponentEvent ce) {
+				jsObject = createCanvas(getId());
 				widgetAttached = true;
+				listener.onCanvasInitialized();
+				
+				addOutline();
 			}
-		} );
+		});
 	}
-	
-	public Node addNode( String id, String label, int xPos, int yPos, int width, int height )
-	{
-		Node node = new Node( id, label, xPos, yPos, width, height );
+
+	public void addOutline() {
+		add(new MxOutline());
+	}
+
+	public Node addNode(String id, String label, int xPos, int yPos, int width, int height) {
+		Node node = new Node(id, label, xPos, yPos, width, height);
 		return node;
 	};
-	
-	public Connection addConnection( Node source, Node target )
-	{
-		Connection connection = new Connection( source, target );
+
+	public Connection addConnection(Node source, Node target) {
+		Connection connection = new Connection(source, target);
 		return connection;
 	};
 
-	public static native JavaScriptObject setStyle( JavaScriptObject cell, String styleName, String styleValue ) /*-{
+	public static native JavaScriptObject setStyle(JavaScriptObject cell, String styleName, String styleValue) /*-{
 		return $wnd.setStyle(cell, styleName, styleValue);
 	}-*/;
-	
-	private static native JavaScriptObject createCanvas( String id ) /*-{
+
+	private static native JavaScriptObject createCanvas(String id) /*-{
 		return $wnd.createCanvas(id);
 	}-*/;
-	
+
 	public native void zoomIn() /*-{
 		$wnd.executeEditorAction('zoomIn');
 	}-*/;
-	
+
 	public native void zoomOut() /*-{
 		$wnd.executeEditorAction('zoomOut');
 	}-*/;
-	
+
 	public native void actualSize() /*-{
 		$wnd.executeEditorAction('actualSize');
 	}-*/;
-	
+
 	public native void fit() /*-{
 		$wnd.executeEditorAction('fit');
 	}-*/;
-	
+
 	public native void showInNewWindow() /*-{
 		$wnd.executeEditorAction('show');
 	}-*/;
-	
+
 	public native void print() /*-{
 		$wnd.executeEditorAction('print');
 	}-*/;
