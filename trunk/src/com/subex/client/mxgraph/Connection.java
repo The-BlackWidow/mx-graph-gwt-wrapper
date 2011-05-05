012040ba7dda69fdb18d50e0be4eb3fa64bb6b88
senguptasourish@gmail.com <senguptasourish@gmail.com@8d2d0ade-4398-c17c-fa17-588cd22d8ca3>
2011-05-05 13:29:54 +0000

    tooltip

--- trunk/src/com/subex/client/mxgraph/Connection.java
+++ trunk/src/com/subex/client/mxgraph/Connection.java
@@ -2,15 +2,13 @@ package com.subex.client.mxgraph;
 
 import com.google.gwt.core.client.JavaScriptObject;
 
-public class Connection extends CanvasItem
-{
-	public Connection( Node source, Node target )
-	{
+public class Connection extends CanvasItem {
+	public Connection(Node source, Node target) {
 		id = source.id + "_" + target.id;
-		setJsObject( createConnection( id, "", source.getJsObject(), target.getJsObject() ) );
+		setJsObject(createConnection(id, "", source.getJsObject(), target.getJsObject()));
 	}
 
-	private static native JavaScriptObject createConnection( String id, String label, JavaScriptObject source, JavaScriptObject target ) /*-{
+	private static native JavaScriptObject createConnection(String id, String label, JavaScriptObject source, JavaScriptObject target) /*-{
 		return $wnd.createConnection( id, label, source, target );
 	}-*/;
 }
